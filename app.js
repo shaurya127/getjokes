516ff9bf1a6d0da14eb67bedb8b544dbd55a10a9
SHAURYA PRATAP SINGH <63494206+shaurya127@users.noreply.github.com>
2021-01-13 12:20:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//     .then(res => {
+//         console.log(res)
+//     })
+//     .catch(err => {
+//         console.log("ERROR!", err)
+//     })
+
+// const fetchBitcoinPrice = async () => {
+//     try {
+//         const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+//         console.log(res.data.ticker.price)
+//     } catch (e) {
+//         console.log("ERROR!", e)
+//     }
+// }
+
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
+    }
+
+}
+
+button.addEventListener('click', addNewJoke)
